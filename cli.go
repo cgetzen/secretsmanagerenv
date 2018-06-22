4c1573c561971fb08e98cb7b0cfe3f4bce434490
Charlie Getzen <charlie.getzen@procore.com>
2018-06-22 01:41:44 -0700

    cleanup

--- cli.go
+++ cli.go
@@ -10,6 +10,7 @@ import (
   "os/exec"
   "io"
   // "bytes"
+  // "reflect"
 )
 
 const (
@@ -42,54 +43,43 @@ func printOutput(w io.Writer, r io.Reader) error {
 			return err
 		}
 	}
-	// never reached
 	panic(true)
 	return nil
 }
 
 
 func RunScript(config Config, command []string) error {
-  path := config.secret_path
-  if DEBUG {
-    fmt.Println(path)
-    for _, x := range command {
-      fmt.Println(x)
-    }
+  var sess *session.Session
+  var secrets_json map[string]string
+
+  if region, err := config.Region(); err == nil {
+    sess = session.Must(session.NewSession(&aws.Config{
+    	Region: aws.String(region),
+    }))
+  } else {
+    sess = session.Must(session.NewSession())
   }
 
-  sess := session.Must(session.NewSession(&aws.Config{
-  	Region: aws.String("us-east-1"),
-  }))
-
   svc := secretsmanager.New(sess)
+
   input := &secretsmanager.GetSecretValueInput{
-      SecretId:     aws.String(path),
+      SecretId:     aws.String(config.secret_path),
   }
 
   result, err := svc.GetSecretValue(input)
-
   if err != nil {
     fmt.Println(err.Error())
     return err
   }
 
-  secret_bytes := []byte(*result.SecretString)
-
-  if DEBUG {
-    fmt.Println(secret_bytes)
-  }
-
-  // var f interface{}
-  var f map[string]string
-  err = json.Unmarshal(secret_bytes, &f)
-
+  err = json.Unmarshal([]byte(*result.SecretString), &secrets_json)
   if err != nil {
     fmt.Println(err.Error())
     return err
   }
 
   cmd := exec.Command(command[0], command[1:]...)
-  cmd.Env = append(os.Environ(), map_to_equal_string(f)...)
+  cmd.Env = append(os.Environ(), map_to_equal_string(secrets_json)...)
 
 	stdout, _ := cmd.StdoutPipe()
 	stderr, _ := cmd.StderrPipe()
--- config.go
+++ config.go
@@ -1,7 +1,24 @@
 package main
 
+import (
+  "os"
+  "errors"
+)
+
 
 type Config struct {
   region string
   secret_path string
+}
+
+func (c *Config) Region() (string, error) {
+  if len(c.region) > 0 {
+    return c.region, nil
+  }
+
+  if envVar := os.Getenv("AWS_DEFAULT_REGION"); len(envVar) > 0 {
+    return envVar, nil
+  }
+
+  return "", errors.New("Cannot find region")
 }
